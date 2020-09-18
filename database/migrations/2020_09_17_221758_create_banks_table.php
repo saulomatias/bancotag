bb6bf48670bb666c51d2d3f107a2ac0a0a3fc583
saulomatias <felipechain@gmail.com>
2020-09-18 09:07:34 -0300

    Initial commit

---
+++ database/migrations/2020_09_17_221758_create_banks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBanksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('banks', function (Blueprint $table) {
+            $table->string('codigo');
+            $table->string('nome');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('banks');
+    }
+}
